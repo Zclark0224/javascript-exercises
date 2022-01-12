e3f53690c4b7d746ae7011242384625bf313cdde
Zach Clark <zclark0224@gmail.com>
2022-01-11 22:25:25 -0500

    Complete Javascript Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+if (start < 0 || end < 0) {
+    return "ERROR";
+} else if (Array.isArray(start) || Array.isArray(end)) {
+    return "ERROR";
+} else if (typeof start === 'string' || start instanceof String || typeof end === 'string' || end instanceof String){
+    return "ERROR"
+} else if (start < end) {
+    let sum = 0;
+    while (start <= end) {
+        sum += start;
+        start++; 
+    }
+    return sum;
+} else if (end < start) {
+    let sum = 0;
+    while (end <= start) {
+        sum += end;
+        end++;
+    }
+    return sum;
+}
 };
 
 // Do not edit below this line
