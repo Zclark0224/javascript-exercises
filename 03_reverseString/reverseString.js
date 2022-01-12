065dbc0505092a5bc9b4d6dea3d7c073164c0286
Zach Clark <zclark0224@gmail.com>
2022-01-11 20:50:52 -0500

    Complete Javascript Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
 const reverseString = function(str) {
-
-};
+    const strArray = str.split("");
+    let reversedArray = strArray.reverse();
+    return reversedArray.join("");
+};                                                                                                           
 
 // Do not edit below this line
 module.exports = reverseString;
