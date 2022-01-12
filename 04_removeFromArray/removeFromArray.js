869857af9c0f328404b33feeb219d029089d3705
Zach Clark <zclark0224@gmail.com>
2022-01-11 21:33:24 -0500

    Complete Javascript Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removed, removed2) {
+    let args = Array.prototype.slice.call(arguments);
+    
+    array = array.filter(val => !args.includes(val));
+    return array;
 };
 
 // Do not edit below this line
