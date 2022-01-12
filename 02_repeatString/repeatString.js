84905c5c6c100e2288cb20eae095fc5e8a562d19
Zach Clark <zclark0224@gmail.com>
2022-01-11 23:28:47 -0500

    Complete Javascript Exxercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        while (num > 0) {
+        repeatedString += string;
+        num--;
+        }
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
